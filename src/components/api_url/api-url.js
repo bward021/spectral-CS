b647379c4fddfe284649427318240c5a9ddf25b4
Brandon Ward <brandonward.gk@gmail.com>
2021-03-30 10:03:15 -0600

    update env variables to work while hosted

--- src/components/api_url/api-url.js
+++ src/components/api_url/api-url.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000/";
+export const API_URL = process.env.REACT_APP_API_URL || process.env.REACT_APP_LOCAL;
 
 // https://bw-spectral-cs-be.herokuapp.com/
 
