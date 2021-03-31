f6a4ae0bc352ef0dbe4749ca7b5c2b199313490d
Brandon Ward <brandonward.gk@gmail.com>
2021-03-31 14:07:02 -0600

    update add client trial styles

--- src/components/pages/add-client-trial.js
+++ src/components/pages/add-client-trial.js
@@ -1,40 +1,56 @@
-import React, { useState } from 'react';
-import { useParams, useHistory } from "react-router-dom"
-import Duration from '../add-trial-helpers.js/duration';
-import Frequency from '../add-trial-helpers.js/frequency';
-import Trial from '../add-trial-helpers.js/trial';
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import Duration from "../add-trial-helpers.js/duration";
+import Frequency from "../add-trial-helpers.js/frequency";
+import Trial from "../add-trial-helpers.js/trial";
 
 const AddClientTrial = (props) => {
-  let history = useHistory()
+  let history = useHistory();
   let { slug } = useParams();
-  const [clientId] = useState(slug)
-  const [trialType, setTrialType] = useState("")
+  const [clientId] = useState(slug);
+  const [trialType, setTrialType] = useState("");
 
   const renderComponent = () => {
     if (trialType === "") {
-      return("")
+      return "";
     } else if (trialType === "Trial") {
-      return <Trial id={clientId} />
+      return <Trial id={clientId} />;
     } else if (trialType === "Frequency") {
-      return <Frequency id={clientId} />
+      return <Frequency id={clientId} />;
     } else if (trialType === "Duration") {
-      return <Duration id={clientId} />
+      return <Duration id={clientId} />;
     }
-  }
+  };
 
-  return ( 
-    <div className="add-client-trial-container">
-      <h1>Add Trial</h1>
-      <button onClick={() => {history.push(`/data/${clientId}`)}}>Navigate to Client Data Page</button>
-      <select className="select-trial-type" onChange={(e) => {setTrialType(e.target.value)}} value={trialType}>
-        <option value="" defaultValue>Select Trial Type</option>
-        <option value="Trial">Trial</option>
-        <option value="Duration">Duration</option>
-        <option value="Frequency">Frequency</option>
-      </select>
+  return (
+    <div className="add-trial-big-container">
+      <div className="add-client-trial-container">
+        <h2>Add Trial</h2>
+        <button
+          onClick={() => {
+            history.push(`/data/${clientId}`);
+          }}
+        >
+          Navigate to Client Data Page
+        </button>
+        <select
+          className="select-trial-type"
+          onChange={(e) => {
+            setTrialType(e.target.value);
+          }}
+          value={trialType}
+        >
+          <option value="" defaultValue>
+            Select Trial Type
+          </option>
+          <option value="Trial">Trial</option>
+          <option value="Duration">Duration</option>
+          <option value="Frequency">Frequency</option>
+        </select>
         {renderComponent()}
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default AddClientTrial;
